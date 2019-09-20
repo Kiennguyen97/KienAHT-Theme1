d10945c177ffc00a14917904c2047686ed071517
Kiennguyen97 <vankiennguyen188@gmail.com>
2019-09-20 16:01:59 +0700

    Start-MyTheme 20/09/2019

---
+++ KienAHT/KienTheme1/registration.php
@@ -0,0 +1,8 @@
+
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/KienAHT/KienTheme1',
+    __DIR__
+);
+
